50b4925d01385fa07fb47c45c205ac315f3e7352
Parul <mittalparul80@gmail.com>
2020-02-12 14:13:23 +0530

    Initial Commit

---
+++ TelstraIosProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelstraIosProject
+//
+//  Created by Parul on 12/02/20.
+//  Copyright © 2020 parul.b.mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
